3679af66f07165f8b8689f75e46cacf930661489
mashroofmashru <mashroofvlk@gmail.com>
2023-07-13 12:44:51 -0700

    display added

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import tkinter
 from tkinter import *
 
 window = Tk()
 window.geometry("375x667")
 window.title("Calculator")
+
+
+def display(value):
+    label = tkinter.Label(text=value, bg="#d3d3d3", font=("Arial", 40, "bold"), anchor=tkinter.E, padx=24, fg="#25265E")
+    label.pack(expand=True, fill='both')
+
 
 
 mainloop()
