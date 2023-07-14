a0329617b693be674f970ede3e141550a4bcc841
mashroofmashru <mashroofvlk@gmail.com>
2023-07-14 04:51:41 -0700

    clear button and some operation button functions added

--- main.py
+++ main.py
@@ -6,18 +6,60 @@ window.geometry("370x400")
 window.title("Calculator")
 window.configure(bg="light gray")
 
-buttonclick=0
+
 def button_clicked(btnValue):
-    buttonclick=buttonclick+btnValue
+    global buttonclick
+    buttonclick = buttonclick + str(btnValue)
+    input_text.set(buttonclick)
 
 
+input_text = StringVar()
+
+
+def operatorbtn(opr):
+    operator = opr
+    global buttonclick
+    global oldvalue
+    oldvalue = int(buttonclick)
+    buttonclick = ""
+    input_text.set(buttonclick)
+
+
+def equal_btnclick():
+    global operator
+    global buttonclick
+    global oldValue
+    global newvalue
+    global result
+    newvalue = int(buttonclick)
+
+    if operator == 1:
+        result=oldValue+newvalue
+        input_text.set(result)
+
+    elif operator==2:
+        result=oldValue-newvalue
+        input_text.set(result)
+
+    elif operator==3:
+        result=oldValue*newvalue
+        input_text.set(result)
+
+    elif operator==4:
+        result=oldValue/newvalue
+        input_text.set(result)
+def clear_input():
+    global buttonclick
+    buttonclick = ""
+    input_text.set(buttonclick)
 
 
 # display--------------------------------------------------
 Display_frame = Frame(window)
 Display_frame.pack()
-label = tkinter.Label(Display_frame, text=buttonclick,width=48, bg="light gray", font=("Arial", 40, "bold"), anchor=tkinter.E,
-                      padx=24,pady=30, fg="#000000")
+label = tkinter.Label(Display_frame, textvariable=input_text, width=48, bg="light gray", font=("Arial", 40, "bold"),
+                      anchor=tkinter.E,
+                      padx=24, pady=30, fg="#000000")
 label.pack(expand=True, fill='both')
 
 # button--------------------------------------------------
@@ -26,36 +68,36 @@ button_frame.pack(expand=True, fill="both")
 
 # firstRow-------------------------------------------------
 add = Button(button_frame, text="+", fg="#25265E", width=12, height=3, bd=0, bg="#F5F5F5", cursor="hand2",
-             command=lambda: button_clicked("+")).grid(row=1, column=0, padx=1, pady=1)
+             command=lambda: operatorbtn(1)).grid(row=1, column=0, padx=1, pady=1)
 
 sub = Button(button_frame, text="-", fg="#25265E", width=12, height=3, bd=0, bg="#F5F5F5", cursor="hand2",
-             command=lambda: button_clicked("-")).grid(row=1, column=1, padx=1, pady=1)
+             command=lambda: operatorbtn(2)).grid(row=1, column=1, padx=1, pady=1)
 
 multi = Button(button_frame, text="x", fg="#25265E", width=12, height=3, bd=0, bg="#F5F5F5", cursor="hand2",
-               command=lambda: button_clicked("x")).grid(row=1, column=2, padx=1, pady=1)
+               command=lambda: operatorbtn(3)).grid(row=1, column=2, padx=1, pady=1)
 
 div = Button(button_frame, text="/", fg="#25265E", width=12, height=3, bd=0, bg="#F5F5F5", cursor="hand2",
-             command=lambda: button_clicked("/")).grid(row=1, column=3, padx=1, pady=1)
+             command=lambda: operatorbtn(4)).grid(row=1, column=3, padx=1, pady=1)
 
 # secondRow----------------------------------------------------------
 seven = Button(button_frame, text="7", fg="#25265E", width=12, height=3, bd=0, bg="#F5F5F5", cursor="hand2",
-             command=lambda: button_clicked("7")).grid(row=2, column=0, padx=1, pady=1)
+               command=lambda: button_clicked("7")).grid(row=2, column=0, padx=1, pady=1)
 
 eight = Button(button_frame, text="8", fg="#25265E", width=12, height=3, bd=0, bg="#F5F5F5", cursor="hand2",
-             command=lambda: button_clicked("8")).grid(row=2, column=1, padx=1, pady=1)
+               command=lambda: button_clicked("8")).grid(row=2, column=1, padx=1, pady=1)
 
 nine = Button(button_frame, text="9", fg="#25265E", width=12, height=3, bd=0, bg="#F5F5F5", cursor="hand2",
-               command=lambda: button_clicked("9")).grid(row=2, column=2, padx=1, pady=1)
+              command=lambda: button_clicked("9")).grid(row=2, column=2, padx=1, pady=1)
 
 # ThirdRow-----------------------------------------------
 four = Button(button_frame, text="4", fg="#25265E", width=12, height=3, bd=0, bg="#F5F5F5", cursor="hand2",
-             command=lambda: button_clicked("4")).grid(row=3, column=0, padx=1, pady=1)
+              command=lambda: button_clicked("4")).grid(row=3, column=0, padx=1, pady=1)
 
 five = Button(button_frame, text="5", fg="#25265E", width=12, height=3, bd=0, bg="#F5F5F5", cursor="hand2",
-             command=lambda: button_clicked("5")).grid(row=3, column=1, padx=1, pady=1)
+              command=lambda: button_clicked("5")).grid(row=3, column=1, padx=1, pady=1)
 
 six = Button(button_frame, text="6", fg="#25265E", width=12, height=3, bd=0, bg="#F5F5F5", cursor="hand2",
-               command=lambda: button_clicked("6")).grid(row=3, column=2, padx=1, pady=1)
+             command=lambda: button_clicked("6")).grid(row=3, column=2, padx=1, pady=1)
 
 # fourthRow-----------------------------------------------
 one = Button(button_frame, text="1", fg="#25265E", width=12, height=3, bd=0, bg="#F5F5F5", cursor="hand2",
@@ -69,15 +111,15 @@ three = Button(button_frame, text="3", fg="#25265E", width=12, height=3, bd=0, b
 
 # fifthRow-----------------------------------------------
 zero = Button(button_frame, text="0", fg="#25265E", width=12, height=3, bd=0, bg="#F5F5F5", cursor="hand2",
-             command=lambda: button_clicked("0")).grid(row=5, column=0, padx=1, pady=1)
+              command=lambda: button_clicked("0")).grid(row=5, column=0, padx=1, pady=1)
 dot = Button(button_frame, text=".", fg="#25265E", width=12, height=3, bd=0, bg="#F5F5F5", cursor="hand2",
              command=lambda: button_clicked(".")).grid(row=5, column=1, padx=1, pady=1)
 
 clear = Button(button_frame, text="CA", fg="#25265E", width=12, height=3, bd=0, bg="#F5F5F5", cursor="hand2",
-               command=lambda: button_clicked("clear")).grid(row=5, column=2, padx=1, pady=1)
+               command=lambda: clear_input()).grid(row=5, column=2, padx=1, pady=1)
 
 # equalRow-----------------------------------------------
-div = Button(button_frame, text="=", fg="#25265E", width=12, height=14, bd=0, bg="#F5F5F5", cursor="hand2",
-             command=lambda: button_clicked("=")).grid(row=2,rowspan=4, column=3, padx=1, pady=1)
+equal = Button(button_frame, text="=", fg="#25265E", width=12, height=14, bd=0, bg="#F5F5F5", cursor="hand2",
+               command=lambda: equal_btnclick()).grid(row=2, rowspan=4, column=3, padx=1, pady=1)
 
 mainloop()
